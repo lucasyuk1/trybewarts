543cbb228d75215860638727e4e75b8c35b77537
Vinicius Leme <vinileme@gmail.com>
2022-10-14 16:06:37 -0300

    Requisitos 10 e 11 e adição requisito 3

---
+++ script.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  const email = document.getElementById('email');
+  const senha = document.getElementById('senha');
+  const login = document.getElementById('login');
+  login.addEventListener('click', () => {
+    const iEmail = email.value;
+    const iSenha = senha.value;
+    if (iEmail === 'tryber@teste.com' && iSenha === '123456') {
+      alert('"Olá, Tryber!"');
+    } else {
+      alert('Email ou senha inválidos.');
+    }
+  });
+};
